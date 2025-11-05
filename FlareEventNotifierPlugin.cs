4308ac9665e10d17d8aeff59f25224e368d86fdf
SkebbZ <SkebbZ@users.noreply.github.com>
2025-11-05 11:02:03 +0100

    Initial SPT 4 build

---
+++ FlareEventNotifierPlugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExfilFlare
+{
+    [BepInPlugin("com.SkebbZ.ExfilFlare", "ExfilFlare", "1.0.0")]
+    public class FlareEventNotifierPlugin : BaseUnityPlugin
+    {
+        // Create a static logger instance that other classes can access
+        public static ManualLogSource Log = null!;
+
+        internal void Awake()
+        {
+            Log = Logger;
+
+            new FlarePatch().Enable();
+
+            Log.LogInfo("ExfilFlare has loaded and is patching.");
+        }
+    }
+
+}
---
+++ FlarePatch.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using EFT;
+using EFT.Communications;
+using EFT.Interactive;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using UnityEngine;
+
+namespace ExfilFlare;
+
+public class FlarePatch : ModulePatch
+{
+    private static PropertyInfo _firedPlayerProperty = null!;
+
+    protected override MethodBase GetTargetMethod()
+    {
+        var targetType = typeof(FlareShootDetectorZone);
+        var targetMethod = AccessTools.Method(targetType, "method_1");
+
+        var flareEventType = targetMethod.GetParameters()[0].ParameterType;
+
+        // This is a Property. We use AccessTools.Property to get it.
+        _firedPlayerProperty = AccessTools.Property(flareEventType, "FiredPlayer");
+
+        FlareEventNotifierPlugin.Log.LogInfo("Flare patch target acquired successfully.");
+        return targetMethod;
+    }
+
+    [PatchPrefix]
+    public static void PatchPrefix(object __0) // '__0' is the flareEvent object
+    {
+        // We get the value from the Property and cast it to a Player object.
+        if (_firedPlayerProperty.GetValue(__0) is Player firedPlayer)
+        {
+            if (firedPlayer.IsYourPlayer)
+            {
+                FlareEventNotifierPlugin.Log.LogInfo("Successful flare shot by local player detected! Displaying notification.");
+                NotificationManagerClass.DisplayNotification(new ExfilFlareSuccessNotification());
+            }
+        }
+    }
+}
+
+public class ExfilFlareSuccessNotification : NotificationAbstractClass
+{
+    public ExfilFlareSuccessNotification()
+    {
+        Duration = ENotificationDurationType.Long;
+    }
+
+    public override string Description => "Exfil activated";
+    public override ENotificationIconType Icon => ENotificationIconType.Default;
+    public override Color? TextColor => Color.green;
+}
